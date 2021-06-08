bf676a5829aa2b9c7cfff34f626df6c7014f7b18
nahsooyeon <nahsooyeon@gmail.com>
2021-06-08 18:01:02 +0900

    🎉 설치: express 및 database 연동

--- NAVER_API/server/app.js
+++ NAVER_API/server/app.js
@@ -1,4 +1,11 @@
 import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+import https from "https";
+
+import "dotenv/config";
+
+/* database 연동 */
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
@@ -10,3 +17,12 @@ app.get("/", (req, res) => {
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 실행");
 });
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: process.env.LOCAL_DATABASE_USERNAME,
+  password: process.env.LOCAL_DATABASE_PASSWORD,
+  database: process.env.LOCAL_DATABASE_NAME,
+});
+
+connection.connect();
